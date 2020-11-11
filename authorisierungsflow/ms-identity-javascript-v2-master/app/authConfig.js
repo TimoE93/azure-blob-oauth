7b3e57f4f264c51c9f49c3b6210a507d5cd1d727
Timo Engler <Timo.Engler@scheer-group.com>
2020-11-11 17:01:43 +0100

    Authorization Code Flow

---
+++ authorisierungsflow/ms-identity-javascript-v2-master/app/authConfig.js
@@ -0,0 +1,48 @@
+// Config object to be passed to Msal on creation.
+// For a full list of msal.js configuration parameters, 
+// visit https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
+const msalConfig = {
+    auth: {
+        clientId: "cd14e889-b951-4848-8be2-b052e85a09e3",
+        authority: "https://login.microsoftonline.com/5065cb93-d22b-4f4a-829a-18a7f31e9031",
+        redirectUri: "http://localhost:3000/",
+    },
+    cache: {
+        cacheLocation: "sessionStorage", // This configures where your cache will be stored
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback: (level, message, containsPii) => {
+                if (containsPii) {	
+                    return;	
+                }	
+                switch (level) {	
+                    case msal.LogLevel.Error:	
+                        console.error(message);	
+                        return;	
+                    case msal.LogLevel.Info:	
+                        console.info(message);	
+                        return;	
+                    case msal.LogLevel.Verbose:	
+                        console.debug(message);	
+                        return;	
+                    case msal.LogLevel.Warning:	
+                        console.warn(message);	
+                        return;	
+                }
+            }
+        }
+    }
+};
+
+// Add here the scopes that you would like the user to consent during sign-in
+const loginRequest = {
+    scopes: ["User.Read"]
+};
+
+// Add here the scopes to request when obtaining an access token for MS Graph API
+const tokenRequest = {
+    scopes: ["User.Read", "Mail.Read"],
+    forceRefresh: false // Set this to "true" to skip a cached token and go to the server to get a new token
+};
